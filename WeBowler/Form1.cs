78cbfdd6809726087c39f796e6282a7627610cd6
Tao Xu <tao.xu@sage.com>
2014-10-29 16:03:25 -0400

    Playing Field for pins and bowling ball

--- WeBowler/Form1.cs
+++ WeBowler/Form1.cs
@@ -16,5 +16,26 @@ namespace WeBowler
         {
             InitializeComponent();
         }
+        private System.Drawing.Graphics g;
+        private System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+
+        private System.Drawing.Pen pen1 = new System.Drawing.Pen(Color.Blue, 2F);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+            g.DrawEllipse(pen1, 20, 20, 40, 40);
+            g.DrawEllipse(pen1, 80, 20, 40, 40);
+            g.DrawEllipse(pen1, 140, 20, 40, 40);
+            g.DrawEllipse(pen1, 200, 20, 40, 40);
+            g.DrawEllipse(pen1, 50, 80, 40, 40);
+            g.DrawEllipse(pen1, 110, 80, 40, 40);
+            g.DrawEllipse(pen1, 170, 80, 40, 40);
+            g.DrawEllipse(pen1, 80, 140, 40, 40);
+            g.DrawEllipse(pen1, 140, 140, 40, 40);
+            g.DrawEllipse(pen1, 110, 200, 40, 40);
+
+            g.DrawLine(pen1, 0, 500, 260, 500);
+            g.FillEllipse(myBrush, new Rectangle(110, 480, 40, 40));
+        }
     }
 }
